cc617ff7e3f2d379c553187016cfc554b25d649a
lgrayes <lgrayes@gmail.com>
2020-11-12 04:53:52 -0600

    Update final

--- app.py
+++ app.py
@@ -1,9 +1,3 @@
-%matplotlib inline
-%matplotlib notebook
-from matplotlib import style
-style.use('fivethirtyeight')
-import matplotlib.pyplot as pyplot
-
 import numpy as np
 import pandas as pd 
 import datetime as dt 
@@ -11,7 +5,7 @@ import datetime as dt
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
-from sqlalchemy import create_engine, func, inspect, cast, Date 
+from sqlalchemy import create_engine, func, inspect
 from datetime import date 
 
 from flask import Flask, jsonify
@@ -64,9 +58,9 @@ def precipitation():
     # Create our session (link) from Python to the DB
     session = Session(engine)
 
-    df2 = pd.read_sql("SELECT prcp, date FROM  measurement")
-    prcp_dict_list = df2.set_index('date').T.to_dict('records')
-    prcp_dict = prcp_dict_list[0]
+    df2 = pd.read_sql("SELECT date, prcp FROM  measurement")
+    prcp = df2.set_index('date').T.to_dict('records')
+    prcp_dict = prcp[0]
 
     return jsonify(prcp_dict)
 
@@ -93,26 +87,26 @@ def passengers():
     return jsonify(all_stations)
 
 @app.route("/api/v1.0/tobs")
-def passengers():
-    # Create our session (link) from Python to the DB
-    session = Session(engine)
+# def passengers():
+#     # Create our session (link) from Python to the DB
+#     session = Session(engine)
 
-    """Return a list of passenger data including the name, age, and sex of each passenger"""
-    # Query all stations
-    all_stations = session.query(Station.station, Passenger.age, Passenger.sex).all()
+#     """Return a list of passenger data including the name, age, and sex of each passenger"""
+#     # Query all stations
+#     all_stations = session.query(Station.station, Passenger.age, Passenger.sex).all()
 
-    session.close()
+#     session.close()
 
-    # Create a dictionary from the row data and append to a list of all_passengers
-    all_passengers = []
-    for name, age, sex in results:
-        passenger_dict = {}
-        passenger_dict["name"] = name
-        passenger_dict["age"] = age
-        passenger_dict["sex"] = sex
-        all_passengers.append(passenger_dict)
+#     # Create a dictionary from the row data and append to a list of all_passengers
+#     all_passengers = []
+#     for name, age, sex in results:
+#         passenger_dict = {}
+#         passenger_dict["name"] = name
+#         passenger_dict["age"] = age
+#         passenger_dict["sex"] = sex
+#         all_passengers.append(passenger_dict)
 
-    return jsonify(all_passengers)
+#     return jsonify(all_passengers)
 
 if __name__ == '__main__':
     app.run(debug=True)
